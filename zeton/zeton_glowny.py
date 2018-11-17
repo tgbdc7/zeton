206a1c08a0a5a81783a58749b6837beee34104ac
martyna <pokojskamartyna@gmail.com>
2018-11-17 15:36:46 +0100

    formularz dodawania punktow

--- zeton/zeton_glowny.py
+++ zeton/zeton_glowny.py
@@ -4,10 +4,15 @@ Aplikacja: system żetonowy
 """
 
 from flask import Flask
+import json
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World xx!'
+
+def dodaj_punkt(nowe_punkty):
+    return nowe_punkty
+
 
 app.run()
