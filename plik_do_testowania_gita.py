feebbd090cf0e5f478343ac13ef0be4c8ffc89c4
leszek <leeonx@wp.pl>
2018-11-17 19:05:45 +0100

    nowy branch - moj

--- plik_do_testowania_gita.py
+++ plik_do_testowania_gita.py
@@ -5,11 +5,11 @@ from flask import Flask
 app = Flask(__name__)
 counter = 0
 
-# @app.route('/')
-# def hello():
-#     global counter
-#     counter += 1
-#     return f'Hello, World! odwiedzono: {counter} razy.'
+@app.route('/')
+def hello():
+        global counter
+        counter += 1
+        return f'Hello, World! odwiedzono: {counter} razy.'
 
 @app.route('/')
 def cos ():
