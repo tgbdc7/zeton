dedbe527c101946bfba3831684692c5e94a2fe36
leszek <leeonx@wp.pl>
2018-11-17 18:28:11 +0100

    dodanie funkcji wczytaj i zapisz dane

--- zeton/zeton_glowny.py
+++ zeton/zeton_glowny.py
@@ -8,15 +8,35 @@ from flask import Flask, redirect, render_template
 from flask import request, url_for
 import json
 
-punkty = None
 
 app = Flask(__name__)
 
-print("xxxx")
 
 @app.route('/')
 def hello():
-    global punkty
+
+    punkty = wczytaj_dane()
+
+    return render_template('dodaj_punkty.html', punkty=punkty)
+
+
+@app.route("/wszystkie-posty", methods=['POST', 'GET'])
+
+def dodaj_punkt():
+    if request.method == 'POST':
+        try:
+            nowe_punkty = int(request.form['liczba_punktow'])
+            punkty = wczytaj_dane()
+            punkty += nowe_punkty
+            zapisz_dane(punkty)
+        except:
+            pass
+        finally:
+            return redirect(url_for('hello'))
+
+
+def wczytaj_dane():
+
     try:
         with open('dane.json', 'r') as plik:
             punkty = json.load(plik)
@@ -24,21 +44,16 @@ def hello():
         with open('dane.json', 'w') as plik:
             punkty = 0
             json.dump(punkty, plik)
-    return render_template('dodaj_punkty.html', punkty=punkty)
+    return punkty
 
+def zapisz_dane (punkty):
 
-@app.route("/wszystkie-posty", methods=['POST', 'GET'])
-def dodaj_punkt():
-    if request.method == 'POST':
-        try:
-            nowe_punkty = int(request.form['liczba_punktow'])
-            punkty += nowe_punkty
-            with open('dane.json', 'w') as plik:
-                json.dump(punkty, plik)
-        except:
-            pass
-        finally:
-            return redirect(url_for('hello'))
+    try:
+        with open('dane.json','w') as plik:
+            json.dump(punkty,plik)
+    except:
+        return f'Nie można zapisać dancyh do pliku'
+
 
 
 if __name__ == '__main__':
