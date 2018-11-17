f2ac71a0b86c002de2591bde6eab029d226cda68
leszek <leeonx@wp.pl>
2018-11-17 15:48:53 +0100

    dodoano json

--- plik_do_testowania_gita.py
+++ plik_do_testowania_gita.py
@@ -1,6 +1,19 @@
 # plik gdzie testuje wysyłanie zmian na Githuba
 # można modyfikowac :) LM
+from flask import Flask
 
+app = Flask(__name__)
+counter = 0
 
-print(" Nasz projekt !")
-print('hello Leszek')
+# @app.route('/')
+# def hello():
+#     global counter
+#     counter += 1
+#     return f'Hello, World! odwiedzono: {counter} razy.'
+
+@app.route('/')
+def cos ():
+    return "A"
+
+if __name__ == "__main__":
+    app.run()
--- zeton/zeton_glowny.py
+++ zeton/zeton_glowny.py
@@ -4,10 +4,23 @@ Aplikacja: system żetonowy
 """
 
 from flask import Flask
+import json
+
+punkty = 16
+with open('dane.json', 'w') as plik:
+    json.dump(punkty,plik)
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World xx!'
+def hello():
+    global punkty
+    with open('dane.json', 'r') as plik:
+        punkty = json.load(plik)
 
-app.run()
+
+    return f'Aktualnie masz {punkty} punktów!'
+
+
+if __name__ == '__main__':
+    app.run()
