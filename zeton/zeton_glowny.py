8c6791104d294d8daefa8a9d0201a17163930e39
tgbdc7 <tgb.dc7@gmail.com>
2018-11-25 22:48:13 +0100

    new branch - zmiana_struktury_pliku_json-dane (#4)
    Uwaga - przed pierwszym uruchomieniem nalezy usunąć plik "dane.json"

--- zeton/zeton_glowny.py
+++ zeton/zeton_glowny.py
@@ -1,5 +1,5 @@
 """
-Aplikacja: system żetonowy
+Aplikacja: system żetonowy ucznia/dziecka
 
 """
 
@@ -12,22 +12,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    punkty = wczytaj_dane()
+    uczen = wczytaj_dane()
+    punkty = uczen["punkty"]
 
     return render_template('dodaj_punkty.html', punkty=punkty)
 
 
 @app.route("/wszystkie-posty", methods=['POST', 'GET'])
 def dodaj_punkt():
-    #if request.method == 'GET':
+    # if request.method == 'GET':
     #    mozliwe_punkty = [0, 1, 3]
     if request.method == 'POST':
         try:
             nowe_punkty = int(request.form['liczba_punktow'])
             if nowe_punkty > 0:
-                punkty = wczytaj_dane()
-                punkty += nowe_punkty
-                zapisz_dane(punkty)
+                uczen = wczytaj_dane()
+                uczen["punkty"] += nowe_punkty
+                zapisz_dane(uczen)
 
         except:
             pass
@@ -36,20 +37,30 @@ def dodaj_punkt():
 
 
 def wczytaj_dane():
+    """
+    Wczytuje dane z pliku dane.json
+    Jeśli nie ma tego pliku to go tworzy z ustawionymi wartościmai na "0"
+    :return: dane ucznia
+    """
     try:
         with open('dane.json', 'r') as plik:
-            punkty = json.load(plik)
+            dane = json.load(plik)
     except FileNotFoundError:
         with open('dane.json', 'w') as plik:
-            punkty = 0
-            json.dump(punkty, plik)
-    return punkty
+            dane = {"punkty": 0, "szkolny_rekord_tygodnia": 0}
+            json.dump(dane, plik)
+    return dane
 
 
-def zapisz_dane(punkty):
+def zapisz_dane(dane):
+    """
+    zapisuje dane do pliku "dane.json"
+    :param dane: dane ucznia - słownik (dictionary)
+    :return: None
+    """
     try:
         with open('dane.json', 'w') as plik:
-            json.dump(punkty, plik)
+            json.dump(dane, plik)
     except:
         return f'Nie można zapisać dancyh do pliku'
 
@@ -59,10 +70,10 @@ def wykorzystaj_punkty():
     if request.method == 'POST':
         try:
             punkty_do_wykorzystania = int(request.form['liczba_punktow'])
-            punkty = wczytaj_dane()
-            if punkty >= punkty_do_wykorzystania:
-                punkty -= punkty_do_wykorzystania
-                zapisz_dane(punkty)
+            uczen = wczytaj_dane()
+            if uczen["punkty"] >= punkty_do_wykorzystania:
+                uczen["punkty"] -= punkty_do_wykorzystania
+                zapisz_dane(uczen)
             else:
                 print("Niestety nie masz wystarczajaco duzo punktow na ta nagrode")
         except:
