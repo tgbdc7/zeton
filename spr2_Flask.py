c8458d070874f92849e80714c2cd09ed59eb4338
martyna <pokojskamartyna@gmail.com>
2019-01-27 16:36:02 +0100

    dodanie schema.sql - zawiera instrukcje języka SQL tworzące tabele

--- spr2_Flask.py
+++ spr2_Flask.py
@@ -1,12 +1,28 @@
-from flask import Flask
-app = Flask(__name__)
+import pyglet
+import datetime
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+window = pyglet.window.Window(300, 200, caption='Pyglet Clock')
 
-#app.run()
+czas_warna = pyglet.text.Label('--:--',
+                               font_name='Arial',
+                               font_size=30,
+                               anchor_x='center',
+                               anchor_y='center',
+                               x=window.width / 2,
+                               y=window.height / 2,
+                               )
 
 
+@window.event
+def on_draw():
+    window.clear()
+    czas_warna.draw()
 
 
+def update_time(dt):
+    czas_warna.text = datetime.datetime.now().strftime('%M:%S')
+
+
+pyglet.clock.schedule_interval(update_time, 1)
+
+pyglet.app.run()
