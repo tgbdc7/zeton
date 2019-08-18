8b232fa68027f529f0f381831239d48c00658b3d
Sandra Kuczynska <san.kuczynska@gmail.com>
2019-08-18 20:47:04 +0200

    iss_31 - add api user.py (not done)

--- zeton/views.py
+++ zeton/views.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, abort, g, request, flash, get_flas
 from . import auth
 from zeton.data_access import users, prizes, tasks
 
-from werkzeug.security import generate_password_hash, check_password_hash
+
 
 bp = Blueprint('views', __name__)
 
@@ -53,40 +53,15 @@ def child(child_id):
 
     return render_template('child_info.html', **context)
 
-@bp.route('/settings/', methods=['GET'])
+@bp.route('/settings/')
 @auth.login_required
 def user_settings():
-
     users.load_logged_in_user_data()
     logged_user_id = g.user_data['id']
     user_data = users.get_user_data(logged_user_id)
 
     context = {'user_data': user_data}
-    messages = get_flashed_messages()
+    # messages = get_flashed_messages()
 
-    return render_template('user_settings.html', **context, messages=messages)
-
-    # if request.method == 'POST':
-    #
-    #     users.load_logged_in_user_data()
-    #     logged_user_id = g.user_data['id']
-    #     logged_user_password = g.user_data['password']
-    #     user_data = users.get_user_data(logged_user_id)
-    #
-    #     password = request.form['password']
-    #     new_password = request.form['new_password']
-    #     repeat_new_password = request.form['repeat_new_password']
-    #
-    #     hashed_new_password = generate_password_hash(new_password)
-    #
-    #     if new_password == repeat_new_password:
-    #         if user_data:
-    #             if check_password_hash(logged_user_password, password):
-    #                 users.update_password(logged_user_id, hashed_new_password)
-    #                 flash('Nowe hasło wprowadzone poprawnie')
-    #             else:
-    #                 flash('Aktualne hasło zostało źle wprowadzone. Spróbuj ponownie')
-    #     else:
-    #         flash('Nowe hasło i powtórzone nowe hasło muszą się zgadzać. Spróbuj ponownie')
-    #
-    #     return redirect('/settings/')
+    # return render_template('user_settings.html', **context, messages=messages)
+    return render_template('user_settings.html', **context)
