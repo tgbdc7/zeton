d1ef0951cd021027979e6abb8973f3f4610e2bc5
Patryk Lomza <p.lomza@outlook.com>
2019-08-26 13:52:25 +0200

    Fix problem with bans_name after adding new child
    Now new childs data is added to bans_name table on creating new child

--- zeton/api/user.py
+++ zeton/api/user.py
@@ -2,9 +2,10 @@ from flask import request, url_for, g
 from werkzeug.security import generate_password_hash
 from werkzeug.utils import redirect
 
-from data_access import users
 from zeton.api import bp
-from data_access.users import add_new_user, associate_child_with_caregiver, get_child_id
+from zeton.data_access import users
+from zeton.data_access.bans import insert_all_default_bans
+from zeton.data_access.users import add_new_user, get_child_id, associate_child_with_caregiver
 
 
 @bp.route("/user", methods=['POST'])
@@ -28,6 +29,7 @@ def register():
             child_id = get_child_id(username)
             caregiver_id = g.user_data['id']
             associate_child_with_caregiver(caregiver_id, child_id)
+            insert_all_default_bans(child_id)
         except Exception as ex:
             print(ex)
             return {'message': 'Bad request'}, 400
