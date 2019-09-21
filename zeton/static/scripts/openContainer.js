9e98b2bb1621ad634f3915706270f188d7ab712f
Paweł <pawel.nieczuja@onet.eu>
2019-09-21 10:19:31 +0200

    Update openContainer js code

--- zeton/static/scripts/openContainer.js
+++ zeton/static/scripts/openContainer.js
@@ -1,13 +1,13 @@
 'use strict'
 
-document.addEventListener("DOMContentLoaded", function() {
-    
-    if (window.matchMedia("(max-width: 575.99px)").matches) {
+document.addEventListener("DOMContentLoaded", function () {
 
-        const container = document.getElementsByClassName('add-points')[0];
-        const target = container.querySelectorAll("[data-switch-target]");
-        const array = ['one', 'two', 'three', 'four', 'five', 'six'];
+    const viewport = window.matchMedia('(max-width: 575.99px)');
+    const container = document.getElementsByClassName('add-points')[0];
+    const target = container.querySelectorAll("[data-switch-target]");
+    const array = ['one', 'two', 'three', 'four', 'five', 'six'];
 
+    function switcher() {
         target[0].addEventListener('click', switchContainer);
         target[2].addEventListener('click', switchContainer);
         target[4].addEventListener('click', switchContainer);
@@ -15,18 +15,64 @@ document.addEventListener("DOMContentLoaded", function() {
         function switchContainer() {
             const x = this;
             const dataNumber = x.getAttributeNode("data-switch-target").value;
-            const targetNumber = array.indexOf(dataNumber);            
+            const targetNumber = array.indexOf(dataNumber);
 
-            target[targetNumber].style.display = "none";
-            target[targetNumber + 1].style.display = "flex";
+            if (viewport.matches) {
+                //console.log("mały ekran2");
+                target[targetNumber].style.display = "none";
+                target[targetNumber + 1].style.display = "flex";
 
-            for (let i = 0; i < array.length; i++) {
+                for (let i = 0; i < array.length; i++) {
 
-                if (i !== targetNumber && i !== targetNumber + 1) {
-                    target[i].style.display = "flex";
-                    target[++i].style.display = "none";
+                    if (i !== targetNumber && i !== targetNumber + 1) {
+                        target[i].style.display = "flex";
+                        target[++i].style.display = "none";
+                    }
                 }
+
+            } else {
+                //console.log("duży ekran2");
+                freshLarge();
             }
         }
     }
+
+    function freshLarge() {
+        for (let i = 0; i < array.length; i++) {
+            target[i].style.display = "flex";
+        }
+    }
+
+    function freshSmall() {
+        for (let i = 0; i < array.length; i++) {
+            target[i].style.display = "flex";
+            target[++i].style.display = "none";
+        }
+    }
+
+    function screenTest(viewport) {
+        if (viewport.matches) {
+            //console.log("mały ekran");
+            freshSmall();
+            switcher();            
+
+        } else {
+            //console.log("duży ekran");
+            freshLarge();
+        }
+    }
+
+    screenTest(viewport);
+
+    viewport.addListener(screenTest);
+
 });
+
+/*
+if (viewport.matches) {
+        console.log('jest mały');
+        switcher();
+    }
+
+    
+*/
