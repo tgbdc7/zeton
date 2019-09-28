291929728bf29ca2bb6b91e1a4480838806e70d4
Sandra_Kuczynska <san.kuczynska@gmail.com>
2019-09-28 08:31:43 +0200

    change function in data_access

--- zeton/data_access/users.py
+++ zeton/data_access/users.py
@@ -115,8 +115,8 @@ def update_firstname(user_id, new_firstname):
     get_db().cursor().execute(query, params)
     get_db().commit()
 
-def get_caregiver_id_and_role_by_username(username):
-    query = 'select id, role from users where username = ? and role = "caregiver"'
+def get_username_id_and_role_by_username(username):
+    query = 'select id, role from users where username = ?'
     result = get_db().execute(query, (username,))
     row = result.fetchone()
     if row:
