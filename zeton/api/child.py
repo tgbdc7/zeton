1aa2471609a143fa6f389ab076500adc6989816b
Sandra_Kuczynska <san.kuczynska@gmail.com>
2019-10-02 12:56:37 +0200

    add api

--- zeton/api/child.py
+++ zeton/api/child.py
@@ -1,4 +1,5 @@
 from flask import request, redirect, url_for, abort, g, flash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 import zeton.data_access.bans
 import zeton.data_access.points
@@ -54,3 +55,52 @@ def use_points(child_id):
             flash(f'Do tej nagrody brakuje Ci:  {missing_points} {points_word}')
 
     return redirect(return_url)
+
+
+@bp.route("/settings/<child_id>/set_password", methods=['POST'])
+@auth.login_required
+@auth.caregiver_only
+def set_child_password(child_id):
+    logged_user_id = g.user_data['id']
+    child = users.get_child_data(child_id)
+    child_password = child['password']
+
+    password = request.form['password']
+    new_password = request.form['new_password']
+    repeat_new_password = request.form['repeat_new_password']
+
+    hashed_new_password = generate_password_hash(new_password)
+
+    if not logged_user_id:
+        return abort(403)
+
+    if not (password == '' or new_password == '' or repeat_new_password == ''):
+        if new_password == repeat_new_password:
+            if auth.password_validation(new_password):
+                if check_password_hash(child_password, password):
+                    users.update_password(child['id'], hashed_new_password)
+                    flash('Nowe hasło wprowadzone poprawnie')
+                flash('Aktualne hasło zostało źle wprowadzone. Spróbuj ponownie')
+            flash('Hasło musi zawierać 1 dużą literę, 1 małą literę, 1 cyfrę i musi mieć długość 8 znaków')
+        flash('Nowe hasło i powtórzone nowe hasło muszą się zgadzać. Spróbuj ponownie')
+    else:
+        flash('Wypełnij wszystkie pola')
+
+    return redirect(url_for('views.child_password_change', child_id=child_id))
+
+
+@bp.route('/settings/<child_id>/set_firstname', methods=['POST'])
+@auth.login_required
+@auth.caregiver_only
+def set_child_firstname(child_id):
+    child = users.get_child_data(child_id)
+
+    new_firstname = request.form.get('new_firstname')
+
+    if new_firstname:
+        users.update_firstname(child_id, new_firstname)
+    else:
+        flash('Wprowadź imię')
+    flash('Imię zostało zmienione')
+
+    return redirect(url_for('views.child_firstname_change', child_id=child_id))
