42121f37ca99f890e426f32cbdce27493eaf0caf
Sandra_Kuczynska <san.kuczynska@gmail.com>
2019-10-02 12:56:17 +0200

    add views

--- zeton/views.py
+++ zeton/views.py
@@ -30,7 +30,9 @@ def index():
         child_points = points.get_child_points(child['id'])
         childs_tasks = tasks.get_tasks(logged_user_id)
         childs_prizes = prizes.get_prizes(logged_user_id)
-        context = {'child': child, 'child_points': child_points, 'childs_tasks': childs_tasks,
+        context = {'child': child,
+                   'child_points': child_points,
+                   'childs_tasks': childs_tasks,
                    'childs_prizes': childs_prizes}
 
     messages = get_flashed_messages()
@@ -52,7 +54,8 @@ def child(child_id):
                'childs_tasks': childs_tasks,
                'childs_prizes': childs_prizes,
                'role': role,
-               'child_points': child_points}
+               'child_points': child_points
+               }
 
     messages = get_flashed_messages()
 
@@ -85,6 +88,16 @@ def user_settings():
     return render_template('user_settings.html')
 
 
+@bp.route('/child-settings/<child_id>')
+@auth.login_required
+def child_settings(child_id):
+    child = users.get_child_data(child_id)
+
+    context = {'child': child}
+
+    return render_template('child_settings.html', **context)
+
+
 @bp.route('/settings/password')
 @auth.login_required
 def password_change():
@@ -97,6 +110,23 @@ def password_change():
     return render_template('password_change.html', **context, messages=messages)
 
 
+@bp.route('/child-settings/<child_id>/password')
+@auth.login_required
+@auth.caregiver_only
+def child_password_change(child_id):
+    logged_user_id = g.user_data['id']
+    user_data = users.get_user_data(logged_user_id)
+    child = users.get_child_data(child_id)
+
+    context = {'child': child,
+               'user_data': user_data
+               }
+
+    messages = get_flashed_messages()
+
+    return render_template('child_password_change.html', **context, messages=messages)
+
+
 @bp.route('/settings/firstname')
 @auth.login_required
 def firstname_change():
@@ -107,6 +137,22 @@ def firstname_change():
     messages = get_flashed_messages()
 
     return render_template('firstname_change.html', **context, messages=messages)
+
+
+@bp.route('/child-settings/<child_id>/firstname')
+@auth.login_required
+@auth.caregiver_only
+def child_firstname_change(child_id):
+    child = users.get_child_data(child_id)
+    logged_user_id = g.user_data['id']
+    user_data = users.get_user_data(logged_user_id)
+
+    context = {'child': child,
+               'user_data': user_data}
+
+    messages = get_flashed_messages()
+
+    return render_template('child_firstname_change.html', **context, messages=messages)
 
 
 @bp.route('/prizes_detail/<child_id>')
