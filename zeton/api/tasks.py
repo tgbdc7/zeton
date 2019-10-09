51df5a9f5260065c0ff5e1a72bef7eebc30bf7f7
Sandra_Kuczynska <san.kuczynska@gmail.com>
2019-10-09 13:02:01 +0200

    change in api update tasks

--- zeton/api/tasks.py
+++ zeton/api/tasks.py
@@ -1,6 +1,6 @@
 from flask import request, redirect, flash
 
-from zeton.data_access.tasks import delete_childs_task, add_new_task, update_task
+from zeton.data_access.tasks import delete_childs_task, add_new_task, update_task, get_task_id_by_name
 
 from zeton import auth
 from zeton.api import bp
@@ -41,16 +41,19 @@ def add_task(child_id):
 @bp.route("/child/<child_id>/tasks/update", methods=['POST'])
 @auth.login_required
 @auth.caregiver_only
-def update_task(child_id, task_id):
+def update_tasks(child_id):
     child_id = int(child_id)
 
+    tasks_name = request.form['tasks_name']
     name = request.form['name']
     points = request.form['points']
     max_day = request.form['max_day']
     max_week = request.form['max_week']
 
+    tasks_id = get_task_id_by_name(child_id, tasks_name)
+
     if not (name == '' or points == '' or max_day == '' or max_week == ''):
-        update_task(child_id, name, points, max_day, max_week, 1, task_id)
+        update_task(child_id, name, points, max_day, max_week, 1, tasks_id)
         flash('Zadanie zostało zmienione')
     else:
         flash('Wypełnij wszystkie pola')
