4395aa591052ec42ad701d48b4ce86656d0038b0
Sandra_Kuczynska <san.kuczynska@gmail.com>
2019-10-09 08:56:25 +0200

    add api to add prizes

--- zeton/api/prizes.py
+++ zeton/api/prizes.py
@@ -1,6 +1,6 @@
-from flask import request, redirect
+from flask import request, redirect, flash
 
-from zeton.data_access.prizes import delete_childs_prize
+from zeton.data_access.prizes import delete_childs_prize, add_new_prize
 
 
 from zeton import auth
@@ -19,3 +19,23 @@ def delete_prizes(child_id, prizes_id):
 
     return redirect(request.referrer)
 
+
+@bp.route("/child/<child_id>/prizes/add", methods=['POST'])
+@auth.login_required
+@auth.caregiver_only
+def add_prizes(child_id):
+    child_id = int(child_id)
+
+    name = request.form['name']
+    points = int(request.form['points'])
+    max_day = int(request.form['max_day'])
+    max_week = int(request.form['max_week'])
+    max_month = int(request.form['max_month'])
+
+    if not (name == '' or points == '' or max_day == '' or max_week == '' or max_month == ''):
+        add_new_prize(child_id, name, points, max_day, max_week, 1)
+        flash('Nagroda została dodana')
+    else:
+        flash('Wypełnij wszystkie pola')
+
+    return redirect(request.referrer)
