2bccd931e96e9f97e1fc79e49116049dcf75b883
Sandra_Kuczynska <san.kuczynska@gmail.com>
2019-10-09 08:43:50 +0200

    add function delete_childs_prizes to data_access

--- zeton/data_access/prizes.py
+++ zeton/data_access/prizes.py
@@ -14,3 +14,11 @@ def create_prize(user_id, name, points, max_day, max_week, max_month):
     params = (user_id, name, points, max_day, max_week, max_month)
     cur.execute(query, params)
     get_db().commit()
+
+
+def delete_childs_prizes(child_id, prizes_id):
+    query = "DELETE FROM prizes WHERE user_id = ? AND id = ?"
+    cur = get_db().cursor()
+    params = (child_id, prizes_id)
+    cur.execute(query, params)
+    get_db().commit()
