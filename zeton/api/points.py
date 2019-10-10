45215ce948dceba4dd8fc2b196be920ecb8404b8
Hanka <hankakokocinska@wp.pl>
2019-10-10 21:34:57 +0200

    add to child view - add points and giving bans

--- zeton/api/points.py
+++ zeton/api/points.py
@@ -1,17 +1,18 @@
-from flask import request, redirect, url_for, abort, g, flash
+from flask import request, redirect, url_for, g, flash
 
 import zeton.data_access.bans
 import zeton.data_access.points
 from zeton import auth
 from zeton.api import bp
-from zeton.data_access import users
+
 
 
 @bp.route("/child/<child_id>/points/add", methods=['POST'])
 @auth.login_required
-@auth.caregiver_only
+@auth.logged_child_or_caregiver_only
 def add_points(child_id):
     logged_user_id = g.user_data['id']
+    return_url = request.args.get('return_url', '/')
 
     try:
         added_points = int(request.form['liczba_punktow'])
@@ -23,8 +24,11 @@ def add_points(child_id):
         zeton.data_access.points.change_points_by(child_id, added_points, logged_user_id)
         zeton.data_access.points.add_exp(added_points, child_id)
 
-    return redirect(url_for('views.child', child_id=child_id))
-
+    role = g.user_data['role']
+    if role == 'child':
+        return redirect(return_url)
+    else:
+        return redirect(url_for('views.child', child_id=child_id))
 
 @bp.route("/child/<child_id>/points/use", methods=['POST'])
 @auth.login_required
