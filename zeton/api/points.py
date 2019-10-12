66923df1165399dcdc09c7ac83bd7ae9821cee70
Marcin <biz.marcin@gmail.com>
2019-10-12 13:32:26 +0200

    add points button ready to check

--- zeton/api/points.py
+++ zeton/api/points.py
@@ -8,32 +8,31 @@ from zeton.api import bp
 from datetime import datetime
 from zeton.data_access import users
 
-def max_day(child_id, exercise_id):
-    now = datetime.now()
-    dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
-    history=zeton.data_access.points.get_points_history_limits(child_id, dt_string)
-    day_limit=zeton.data_access.points.get_ex_day_limit(exercise_id)
-    if(history.count<day_limit):
-        return True
-    else:
-        return False
+# def max_day(child_id, exercise_id):
+#     now = datetime.now()
+#     dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+#     history=zeton.data_access.points.get_points_history_limits(child_id, dt_string)
+#     day_limit=zeton.data_access.points.get_ex_day_limit(exercise_id)
+#     if(history.count<day_limit):
+#         return True
+#     else:
+#         return False
 
 @bp.route("/child/<child_id>/points/add/<points>/<ex_id>", methods=['POST'])
 @auth.login_required
 @auth.caregiver_only
 def add_points(child_id,points,ex_id):
-    if(max_day(child_id,ex_id)):
-        logged_user_id = g.user_data['id']
+    logged_user_id = g.user_data['id']
 
-        try:
-            added_points = int(points)
-        except ValueError as ex:
-            print(ex)
-            return {'message': 'Bad request'}, 400
+    try:
+        added_points = int(points)
+    except ValueError as ex:
+        print(ex)
+        return {'message': 'Bad request'}, 400
 
-        if added_points > 0:
-            zeton.data_access.points.change_points_by(child_id, added_points, logged_user_id)
-            zeton.data_access.points.add_exp(added_points, child_id)
+    if added_points > 0:
+        zeton.data_access.points.change_points_by(child_id, added_points, logged_user_id)
+        zeton.data_access.points.add_exp(added_points, child_id)
 
     return redirect(url_for('views.child', child_id=child_id))
 
