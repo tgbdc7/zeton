e60fa3b32863e9baf731fd22b2a7995c1f94ed9c
Marcin <biz.marcin@gmail.com>
2019-10-20 21:23:10 +0200

    79 add buutons to add points (#91)
    added buttons  to add points for the completed task

--- zeton/api/points.py
+++ zeton/api/points.py
@@ -3,19 +3,30 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 import zeton.data_access.bans
 import zeton.data_access.points
+import datetime
 from zeton import auth
 from zeton.api import bp
+from datetime import datetime
 from zeton.data_access import users
 
+# def max_day(child_id, exercise_id):
+#     now = datetime.now()
+#     dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+#     history=zeton.data_access.points.get_points_history_limits(child_id, dt_string)
+#     day_limit=zeton.data_access.points.get_ex_day_limit(exercise_id)
+#     if(history.count<day_limit):
+#         return True
+#     else:
+#         return False
 
-@bp.route("/child/<child_id>/points/add", methods=['POST'])
+@bp.route("/child/<child_id>/points/add/<points>/<ex_id>", methods=['POST'])
 @auth.login_required
 @auth.caregiver_only
-def add_points(child_id):
+def add_points(child_id,points,ex_id):
     logged_user_id = g.user_data['id']
 
     try:
-        added_points = int(request.form['liczba_punktow'])
+        added_points = int(points)
     except ValueError as ex:
         print(ex)
         return {'message': 'Bad request'}, 400
