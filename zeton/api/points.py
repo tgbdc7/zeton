8e18e8e8c3bdf7b488783e5ce5794ac6fe44c25a
Hanka <hankakokocinska@wp.pl>
2019-10-23 14:56:24 +0200

    add api with add points from field

--- zeton/api/points.py
+++ zeton/api/points.py
@@ -22,7 +22,7 @@ from zeton.data_access import users
 @bp.route("/child/<child_id>/points/add/<points>/<ex_id>", methods=['POST'])
 @auth.login_required
 @auth.logged_child_or_caregiver_only
-def add_points(child_id,points,ex_id):
+def add_points_buttons(child_id,points,ex_id):
     logged_user_id = g.user_data['id']
     return_url = request.args.get('return_url', '/')
 
@@ -41,6 +41,31 @@ def add_points(child_id,points,ex_id):
         return redirect(return_url)
     else:
         return redirect(url_for('views.child', child_id=child_id))
+
+@bp.route("/child/<child_id>/points/add", methods=['POST'])
+@auth.login_required
+@auth.logged_child_or_caregiver_only
+def add_points(child_id):
+    logged_user_id = g.user_data['id']
+    return_url = request.args.get('return_url', '/')
+
+    try:
+        added_points = int(request.form['liczba_punktow'])
+    except ValueError as ex:
+        print(ex)
+        return {'message': 'Bad request'}, 400
+
+    if added_points > 0:
+        zeton.data_access.points.change_points_by(child_id, added_points, logged_user_id)
+        zeton.data_access.points.add_exp(added_points, child_id)
+
+    role = g.user_data['role']
+    if role == 'child':
+        return redirect(return_url)
+    else:
+        return redirect(url_for('views.child', child_id=child_id))
+
+
 
 
 @bp.route("/child/<child_id>/points/use", methods=['POST'])
