3b42e5884834f70cc62f412e06929cbca493ac6c
Sandra_Kuczynska <san.kuczynska@gmail.com>
2019-10-23 16:21:47 +0200

    change delete to deactivate

--- zeton/api/prizes.py
+++ zeton/api/prizes.py
@@ -1,20 +1,19 @@
 from flask import request, redirect, flash
 
-from zeton.data_access.prizes import delete_childs_prize, add_new_prize, update_prize
+from zeton.data_access.prizes import deactivate_prize, create_prize, update_prize
 
 from zeton import auth
 from zeton.api import bp
 
 
-@bp.route("/child/<child_id>/prizes/delete/<prizes_id>", methods=['POST'])
+@bp.route("/child/<child_id>/prizes/deactivate/<prizes_id>", methods=['POST'])
 @auth.login_required
 @auth.caregiver_only
-def delete_prizes(child_id, prizes_id):
+def deactivate_prizes(child_id, prizes_id):
     child_id = int(child_id)
-
     prizes_id = int(prizes_id)
 
-    delete_childs_prize(child_id, prizes_id)
+    deactivate_prize(child_id, prizes_id)
 
     return redirect(request.referrer)
 
@@ -26,13 +25,13 @@ def add_prizes(child_id):
     child_id = int(child_id)
 
     name = request.form['name']
-    points = int(request.form['points'])
-    max_day = int(request.form['max_day'])
-    max_week = int(request.form['max_week'])
-    max_month = int(request.form['max_month'])
+    points = request.form['points']
+    max_day = request.form['max_day']
+    max_week = request.form['max_week']
+    max_month = request.form['max_month']
 
     if not (name == '' or points == '' or max_day == '' or max_week == '' or max_month == ''):
-        add_new_prize(child_id, name, points, max_day, max_week, max_month)
+        create_prize(child_id, name, points, max_day, max_week, max_month, 1)
         flash('Nagroda została dodana')
     else:
         flash('Wypełnij wszystkie pola')
--- zeton/data_access/prizes.py
+++ zeton/data_access/prizes.py
@@ -2,34 +2,29 @@ from zeton.db import get_db
 
 
 def get_prizes(child_id):
-    query = "SELECT * FROM prizes WHERE user_id = ? ORDER BY points"
+    query = "SELECT * FROM prizes WHERE user_id = ? AND is_active = 1 ORDER BY points"
     result = get_db().execute(query, (child_id,))
     return result.fetchall()
 
 
-def create_prize(user_id, name, points, max_day, max_week, max_month):
-    query = "INSERT INTO prizes (user_id, name, points, max_day, max_week, max_month) VALUES (?, ?, ?, ?, ?, ?)"
+def create_prize(user_id, name, points, max_day, max_week, max_month, is_active):
+    query = "INSERT INTO prizes " \
+            "(user_id, name, points, max_day, max_week, max_month, is_active) " \
+            "VALUES (?, ?, ?, ?, ?, ?, ?)"
 
     cur = get_db().cursor()
-    params = (user_id, name, points, max_day, max_week, max_month)
+    params = (user_id, name, points, max_day, max_week, max_month, is_active)
     cur.execute(query, params)
     get_db().commit()
 
 
-def delete_childs_prize(child_id, prizes_id):
-    query = "DELETE FROM prizes WHERE user_id = ? AND id = ?"
+def deactivate_prize(child_id, prizes_id):
+    query = "UPDATE 'prizes' " \
+            "SET is_active = 0 " \
+            "WHERE user_id = ? AND id = ? "
     cur = get_db().cursor()
     params = (child_id, prizes_id)
     cur.execute(query, params)
-    get_db().commit()
-
-
-def add_new_prize(user_id, name, points, max_day, max_week, max_month):
-    query = "INSERT INTO 'prizes' " \
-            "(user_id, name, points, max_day, max_week, max_month) " \
-            "VALUES (?, ?, ?, ?, ?, ?) "
-    params = (user_id, name, points, max_day, max_week, max_month)
-    get_db().execute(query, params)
     get_db().commit()
 
 
