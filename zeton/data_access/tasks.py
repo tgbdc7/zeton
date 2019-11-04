a8a24a584ab52c1f72d9a44ebe1e52992426c09a
Sandra Kuczynska <44257228+visse0001@users.noreply.github.com>
2019-11-04 21:53:11 +0100

    124/caregiver can edit tasks (#128)
    add templates end api to edit tasks. Close#125

--- zeton/data_access/tasks.py
+++ zeton/data_access/tasks.py
@@ -15,3 +15,39 @@ def create_task(user_id, name, points, max_day, max_week):
     params = (user_id, name, points, max_day, max_week)
     cur.execute(query, params)
     get_db().commit()
+
+
+def deactivate_task(child_id, task_id):
+    query = "UPDATE 'home_points' " \
+            "SET is_active = 0 " \
+            "WHERE user_id = ? AND id = ? "
+    cur = get_db().cursor()
+    params = (child_id, task_id)
+    cur.execute(query, params)
+    get_db().commit()
+
+
+def add_new_task(user_id, name, points, max_day, max_week, is_active):
+    query = "INSERT INTO 'home_points' " \
+            "(user_id, name, points, max_day, max_week, is_active) " \
+            "VALUES (?, ?, ?, ?, ?, ?) "
+    params = (user_id, name, points, max_day, max_week, is_active)
+    get_db().execute(query, params)
+    get_db().commit()
+
+
+def update_task(user_id, name, points, max_day, max_week, is_active, task_id):
+    query = "UPDATE 'home_points' " \
+            "SET user_id = ?, name = ?, points = ?, max_day = ?, max_week = ?, is_active = ?" \
+            "WHERE id = ?"
+    params = (user_id, name, points, max_day, max_week, is_active, task_id)
+    get_db().cursor().execute(query, params)
+    get_db().commit()
+
+
+def get_task_by_tasks_id(child_id, task_id):
+    query = "SELECT * FROM 'home_points' " \
+            "WHERE user_id = ? AND id = ?"
+    params = (child_id, task_id)
+    result = get_db().execute(query, params)
+    return result.fetchone()
