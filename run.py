93f135f75e65073982737dd97f55636b9d8cba28
Piotrekkw <piotrekkw@yahoo.com>
2019-11-23 17:51:03 +0100

    Merge branch 'master' of C:\Users\piotr\zeton with conflicts.

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ app = zeton.create_app()
 if __name__ == '__main__':
     # if you want test on computer use this command
     port = os.environ.get('PORT', 5000)
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host= '0.0.0.0', port=port, debug=True)
 
     # # If you want test on mobile use this command
     # # also you must use your local ip in your browser
