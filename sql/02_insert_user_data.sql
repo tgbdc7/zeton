3d45d1fec40a5a8f9686a419984ecb751e1a9095
Marcin <biz.marcin@gmail.com>
2019-11-25 18:34:23 +0100

    fix data

--- sql/02_insert_user_data.sql
+++ sql/02_insert_user_data.sql
@@ -1,13 +1,5 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users VALUES(1,'opiekun1','pbkdf2:sha256:50000$Cg0Lvo4Z$8c2301855e36015cd9e40d5983431029641ee5f96f281eacb0b863f64c495a0e','caregiver','Antoni',NULL);
-INSERT INTO users VALUES(2,'dziecko1','pbkdf2:sha256:50000$cnZ5hmIP$288b45f6adfaf99f1ce60560d44c8a34e2b73df87c6ed3e2f96f686a40d0a4cd','child','Bazyli',NULL);
-INSERT INTO users VALUES(3,'dziecko2','pbkdf2:sha256:50000$F4RzVb3Z$e39c6ffd2f78f8a435c4b62c38d1d06be26d3fc4dd77d5d5ecd7e8156432eb3b','child','Celina',NULL);
-INSERT INTO users VALUES(4,'opiekun2','pbkdf2:sha256:50000$UXPZgrr8$0981537e8e9d8c8fdbbd1672244d6fda81513bdcd432cc2f91606686d9775e86','caregiver','Dominika',NULL);
-INSERT INTO users VALUES(5,'dziecko3','pbkdf2:sha256:50000$plsLYnx2$db59410c22b1e5a9bda5e1ee1a74a8f3ff219bb83e7f7ce2de00930006e9673c','child','Ewelina',NULL);
-INSERT INTO users VALUES(6,'dziecko4','pbkdf2:sha256:50000$qYXjlGbk$ffbb4b0157f254d360dd80ebef8895462d3b569362e45108728be2c4672fcd0b','child','Filip',NULL);
-INSERT INTO users VALUES(7,'dyrektor','pbkdf2:sha256:50000$uqVtNBEGIN TRANSACTION;
-
 INSERT INTO users VALUES(1,''opiekun1'',''pbkdf2:sha256:50000$Cg0Lvo4Z$8c2301855e36015cd9e40d5983431029641ee5f96f281eacb0b863f64c495a0e'',''caregiver'',''Antoni'',NULL,''opiekun1@example.com'');
 INSERT INTO users VALUES(2,''dziecko1'',''pbkdf2:sha256:50000$cnZ5hmIP$288b45f6adfaf99f1ce60560d44c8a34e2b73df87c6ed3e2f96f686a40d0a4cd'',''child'',''Bazyli'', NULL, ''dziecko1@example.com'');
 INSERT INTO users VALUES(3,''dziecko2'',''pbkdf2:sha256:50000$F4RzVb3Z$e39c6ffd2f78f8a435c4b62c38d1d06be26d3fc4dd77d5d5ecd7e8156432eb3b'',''child'',''Celina'',NULL,''dziecko2@example.com'');
