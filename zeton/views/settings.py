4002072966b35c90c86f93616c6328c874fdb13a
Patryk Lomza <p.lomza@outlook.com>
2020-01-14 11:46:29 +0100

    Update settings views

--- zeton/views/settings.py
+++ zeton/views/settings.py
@@ -89,31 +89,52 @@ def child_firstname_change(child_id):
 @bp.route('/settings/admin/permissions')
 # @auth.login_required
 def list_family_members():
-    user_id = g.user_data['id']
-    # auth.check_permission(
-    #     user_id, auth.permissions['ADD_POINTS'].get_value())
-    family_id = users.get_family_id(user_id)
-    family_members_by_id = [row['family_member'] for row
-                            in users.get_family_members(family_id)]
+    family_id = users.get_family_id(g.user_data['id'])
+    family_members_by_id = [user['family_member'] for user in  users.get_family_members(family_id)]
+    members_role = [users.get_user_role(user_id) for user_id in
+                    family_members_by_id]
     family_members_by_name = [users.get_username(user_id) for user_id
                               in family_members_by_id]
     family_members_by_name_and_id = zip(family_members_by_name,
                                         family_members_by_id)
-    context = {'family_members_by_id': family_members_by_id,
-               'family_members_by_name': family_members_by_name,
-               'family_members_by_name_and_id': family_members_by_name_and_id}
-    return render_template('permissions.html', **context)
+    family_member_data = {}
+    for member_name in family_members_by_name:
+        family_member_data[member_name] = {}
+    for index, item in enumerate(family_member_data.values()):
+        item['id'] = family_members_by_id[index]
+        item['name'] = family_members_by_name[index]
+        item['role'] = members_role[index]
+    context = {'members_data': family_member_data}
+    return render_template('permissions/permissions.html', **context)
 
 
-@bp.route('/settings/manage-permissions/<int:user_id>')
-def manage_permissions(user_id):
-    session['user_id'] = user_id
+@bp.route('/settings/admin/permissions/<int:user_id>', methods=['GET'])
+@bp.route('/settings/admin/permissions/<int:user_id>/<int:child_id>')
+def manage_permissions(user_id, child_id=None):
+    session['target_user_id'] = user_id
+    session['child_id'] = child_id
     used_permissions = auth.get_used_permissions(
         user_id, auth.permissions)
     unused_permissions = auth.get_unused_permissions(user_id, auth.permissions)
 
+    if users.get_user_role(user_id) in auth.SUPERVISING_ROLES and child_id is None:
+        user_id = session.get('target_user_id')
+        children_under_caregiver = users.get_caregivers_children(user_id)
+        context = {'children_under_caregiver': children_under_caregiver, 'user_id': user_id}
+
+        return render_template('permissions/permissions_children_list.html', **context)
+    if child_id is not None:
+        used_permissions = auth.get_used_permissions(
+            user_id,
+            auth.permissions,
+            assigned_user_id=child_id
+        )
+        unused_permissions = auth.get_unused_permissions(
+            user_id,
+            auth.permissions,
+            assigned_user_id=child_id
+        )
+
     context = {'used_permissions': used_permissions,
                'unused_permissions': unused_permissions}
-    messages = None
-
-    return render_template('permissions_list.html', **context)
+    return render_template('permissions/permissions_list.html', **context)
